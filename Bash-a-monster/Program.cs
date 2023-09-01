f8ff67797da206790879343b2c3bd3bdad36dcdc
James Gill <james.gill@elev.ga.ntig.se>
2023-09-01 10:39:25 +0200

    hej

---
+++ Bash-a-monster/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Välkommen till spelet bash a monster");
+
+public class Hero
+{
+
+    //egenskaper
+
+    public float MaxPlayerHealth = 100;
+    public float PlayerHealth;
+    MaxPlayerHealth = PlayerHealth
+
+
+
+
+
+}
+
+Console.ReadLine();
